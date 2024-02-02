f953428ff0d3017f8031ec0e8a4983c7f434caf9
maybe.havecookies <1059758@apps.nsd.org>
2024-02-02 11:43:20 -0800

    Added descriptions for variables.

--- Core/WumpusCore/Player/Player.cs
+++ Core/WumpusCore/Player/Player.cs
@@ -4,12 +4,23 @@ namespace WumpusCore.Player
 {
     public class Player
     {
+        /// <summary>
+        /// The room the player is currently in.
+        /// </summary>
         public int position { get; private set; }
 
-        private string sprPath = null;
+        /// <summary>
+        /// The path of the player's sprite.
+        /// </summary>
+        private string spritePath = "";
 
-        // Inventory
+        /// <summary>
+        /// The amount of coins the player currently has.
+        /// </summary>
         public int coins { get; set; }
+        /// <summary>
+        /// The amount of arrows the player currently has.
+        /// </summary>
         public int arrows { get; set; }
 
         public Player()
@@ -17,7 +28,7 @@ namespace WumpusCore.Player
             coins = 0;
             arrows = 0;
 
-            if (!File.Exists(sprPath))
+            if (!File.Exists(spritePath))
             {
                 throw new FileNotFoundException();
             }
