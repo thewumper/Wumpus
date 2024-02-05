d2d11e3da50838d4cafd935e8331129466f59a82
ilhs1041495 <113475810+ilhs1041495@users.noreply.github.com>
2024-02-05 11:41:15 -0800

    Finished stub for High Score
    pescado limo el mueble

--- Core/WumpusCore/HighScore/HighScore.cs
+++ Core/WumpusCore/HighScore/HighScore.cs
@@ -1,12 +1,121 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using WumpusCore.Topology;
 
 namespace WumpusCore.HighScore
 {
     internal class HighScore
     {
+        /// <summary>
+        /// name of the player inputted at game beginning or end
+        /// </summary>
+        private string playerName;
+
+        /// <summary>
+        /// once the score is calculated it is stored here
+        /// </summary>
+        private int score;
+
+        /// <summary>
+        /// the number of turns the player took to win
+        /// </summary>
+        private int numTurns;
+
+        /// <summary>
+        /// Gold coins still in player's inventory
+        /// </summary>
+        private int goldLeft;
+
+        /// <summary>
+        /// Arrows still in player's inventory
+        /// </summary>
+        private int arrowsLeft;
+
+        /// <summary>
+        /// Did wumpus get deaded
+        /// </summary>
+        private bool isWumpusDead;
+
+        /// <summary>
+        /// The map that was played from the round
+        /// </summary>
+        private int mapUsed;
+
+        /// <summary>
+        /// The list of storedHighScore structs for the 10 highest scores
+        /// </summary>
+        private StoredHighScore[] top10HighScores;
+        
+        /// <summary>
+        /// HighScore object is how the score of the game
+        /// is calculated and stored to files
+        /// </summary>
+        /// <param name="playerName"></param>
+        /// <param name="numTurns"></param>
+        /// <param name="goldLeft"></param>
+        /// <param name="arrowsLeft"></param>
+        /// <param name="isWumpusDead"></param>
+        /// <param name="mapUsed"></param>
+        public HighScore(string playerName, int numTurns, int goldLeft, int arrowsLeft, bool isWumpusDead, int mapUsed)
+        {
+            // store parameters into the fields for future use
+        }
+
+        /// <summary>
+        /// Score is calculated from the game variables, then stored
+        /// in the score field
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        private int calculateScore()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Turns the bool isWumpusDead into
+        /// an int so the score can be calculated
+        /// using the translated 1 or 0
+        /// </summary>
+        /// <param name="isWumpusDead"></param>
+        /// <returns></returns>
+        private int translateWumpusLife(bool isWumpusDead)
+        {
+            return 0;
+        }
+
+        /// <summary>
+        /// Stores an individual HighScore struct to the file
+        /// </summary>
+        private void storeScoreToFile()
+        {
+            
+        }
+
+        /// <summary>
+        /// Loops through the high score struct list and
+        /// stores them on the file
+        /// </summary>
+        private void storeTopTenToFile()
+        {
+
+        }
+
+        /// <summary>
+        /// Compacted score, stores the score
+        /// along with the player name and the
+        /// variables that allowed the score to
+        /// be achieved
+        /// </summary>
+        struct StoredHighScore
+        {
+            readonly int score;
+            readonly string playerName;
+            readonly int numTurns;
+            readonly int goldLeft;
+            readonly int arrowsLeft;
+            readonly bool isWumpusDead;
+            readonly int mapUsed;
+        }
     }
 }
