5336fcd5ea6e9dd275bb18c93690e7d91fc0d64d
MaximumMaxxx <maxwellwindland@gmail.com>
2024-02-05 11:40:02 -0800

    Added stub of cat

---
+++ Core/WumpusCore/LuckyCat/Cat.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace WumpusCore.LuckyCat
+{
+    public class Cat
+    {
+        /// <summary>
+        /// Is the cat tamed
+        /// </summary>
+        private bool tamed;
+
+        /// <summary>
+        /// The room number that the cat is currently in
+        /// </summary>
+        private int location;
+
+        /// <summary>
+        /// The radius that the cat should be heard from
+        /// </summary>
+        public const int AudibleMewingRadius = 2;
+
+        /// <summary>
+        /// Figure out how many coins it will take to tame the cat
+        /// </summary>
+        /// <returns>The number of coint to tame the cat</returns>
+        public int tame()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Pets the cat
+        /// </summary>
+        public int pet()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
