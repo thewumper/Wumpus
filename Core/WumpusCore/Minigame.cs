7e5b5bd9a6179eaccf5b0a9541c0fdec75e81663
1118422 <1118422@apps.nsd.org>
2024-02-08 10:42:26 -0800

    Minigame Interface

---
+++ Core/WumpusCore/Minigame.cs
@@ -0,0 +1,12 @@
+namespace WumpusCore
+{
+    public interface Minigame
+    {
+        /// <summary>
+        /// Returns whether the player won
+        /// </summary>
+        /// <returns>True if won, False if lost, and null if game not over</returns>
+        bool? reportResult();
+        
+    }
+}
