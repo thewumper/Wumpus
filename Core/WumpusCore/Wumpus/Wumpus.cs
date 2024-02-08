9def737139a468ea444579d99dd54e46fc7fa506
ILHS2000503 <2000503@apps.nsd.org>
2024-02-08 10:19:33 -0800

    Create Wumpus.cs

---
+++ Core/WumpusCore/Wumpus/Wumpus.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WumpusCore.Wumpus
+{
+    internal class Wumpus
+    {
+        /// <summary>
+        /// Current Room Wumpus is in
+        /// </summary>
+        public int Position { get; private set; }
+        /// <summary>
+        /// Current state Wumpus is in
+        /// </summary>
+        /*
+        0: sleep
+        1: moving
+        2: combat
+        3: dead
+         */
+        public int State { get; private set; }
+        public Wumpus()
+        {
+            Position = 0;
+            State = 0;
+        }
+        /// <summary>
+        /// Moves the wumpus randomly
+        /// </summary>
+        public void move(int maxMove)
+        {
+            throw new NotImplementedException();
+
+        }
+        /// <summary>
+        /// mods state and starts a battle changing state stuff
+        /// </summary>
+        public void startBattle()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
