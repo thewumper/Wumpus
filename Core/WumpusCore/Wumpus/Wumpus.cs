23e654c64102993c61f1a6b87435c7ab8a639802
ILHS2000503 <2000503@apps.nsd.org>
2024-02-12 11:20:57 -0800

    Update Wumpus.cs

--- Core/WumpusCore/Wumpus/Wumpus.cs
+++ Core/WumpusCore/Wumpus/Wumpus.cs
@@ -3,36 +3,44 @@
     internal class Wumpus
     {
         /// <summary>
-        /// Current Room Wumpus is in
+        /// Current Room Wumpus is in.  
+        /// Better description coming soon
         /// </summary>
         public int Position { get; private set; }
         /// <summary>
-        /// state of wumups
+        /// state of wumpus.  
+        /// Better description coming soon
         /// </summary>
         public enum State
         {
             Sleeping,
-            Wake,
+            Wake,   
             Wandering,
             Combat,
             FleeingArrow,
             FleeingCombat,
             Dead
         }
+        /// <summary>
+        /// Constructs the Wumpus.  
+        /// Better description coming soon
+        /// </summary>
         public Wumpus()
         {
             Position = 0;
             State currentState = State.Sleeping;
         }
         /// <summary>
-        /// Moves the wumpus randomly
+        /// Moves the wumpus into a connected room randomly a number of times depending on the state.  
+        /// Better description coming soon
         /// </summary>
         public void move(Random rand)
         { 
             throw new NotImplementedException();
         }
         /// <summary>
-        /// mods state and starts a battle changing state stuff
+        /// Changes state and starts minigame, then changes state again based on result of battle.  
+        /// Better description coming soon
         /// </summary>
         public void startBattle()
         {
