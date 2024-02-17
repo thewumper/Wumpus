e7e0756a70949840e97bc8ce95ad4e67ad0e598a
havecookies <1059758@apps.nsd.org>
2024-02-16 18:36:36 -0800

    Update GameLocations.cs
    1. Documentation changes.
    2. rooms is now an array instead of a list.
    3. The position of a room is now its position on the rooms array.
    5. rooms now stores RoomType instead of Room
    4. The Room struct has been removed

--- Core/WumpusCore/GameLocations/GameLocations.cs
+++ Core/WumpusCore/GameLocations/GameLocations.cs
@@ -20,24 +20,28 @@ namespace WumpusCore.GameLocations
         /// <summary>
         /// The list of rooms and their positions.
         /// </summary>
-        private Room[] rooms;
-
-        public GameLocations(int numRooms)
+        private RoomType[] rooms;
+        
+        /// <summary>
+        /// The object that contains most things to do with rooms.
+        /// </summary>
+        /// <param name="numRooms">The total amount of rooms.</param>
+        public GameLocations(ushort numRooms)
         {
-            rooms = new Room[numRooms];
+            rooms = new RoomType[numRooms];
         }
 
         /// <summary>
-        /// Gets a random empty room.
+        /// Gets a random empty room from the <see cref="rooms">rooms</see>.
         /// </summary>
-        /// <returns>A random room of <c>RoomType</c> type <c>Flats</c> from the <c>rooms</c> list.</returns>
+        /// <returns>A random room of <see cref="RoomType">RoomType</see> type <c>Flats</c> from the <see cref="rooms">rooms</see> array.</returns>
         /// <exception cref="InvalidOperationException"></exception>
         public ushort GetEmptyRoom()
         {
             List<ushort> positions = new List<ushort>();
             for (ushort i = 0; i < rooms.Length; i++)
             {
-                if (rooms[i].type == RoomType.Flats)
+                if (rooms[i] == RoomType.Flats)
                 {
                     positions.Add(i);
                 }
@@ -50,79 +54,43 @@ namespace WumpusCore.GameLocations
         }
 
         /// <summary>
-        /// Adds a room to the <c>rooms</c> list.
+        /// Adds a room to the <see cref="rooms">rooms</see> array.
         /// </summary>
-        /// <param name="type">The <c>RoomType</c> type of room to add.</param>
-        /// <param name="pos">The position of the room to add.</param>
+        /// <param name="type">The <see cref="RoomType">RoomType</see> type of room to add.</param>
+        /// <param name="index">The index of the room to be added on the <see cref="rooms">rooms</see> array.</param>
         /// <exception cref="ArgumentException"></exception>
-        public void AddRoom(RoomType type, ushort pos)
+        public void AddRoom(RoomType type, ushort index)
         {
             for (int i = 0; i < rooms.Length; i++)
             {
-                if (pos == rooms[i].pos)
+                if (index == i)
                 {
                     throw new ArgumentException("A room already exists at that position.");
                 }
             }
-            rooms.Add(new Room(type, pos));
+
+            rooms[index] = type;
         }
 
         /// <summary>
-        /// Sets the room at <c>index</c> to another <c>RoomType</c> type.
+        /// Sets the room at <c>index</c> to another <see cref="RoomType">RoomType</see> type.
         /// </summary>
-        /// <param name="type">The <c>RoomType</c> type to set the room to.</param>
-        /// <param name="index">The index of the room on the <c>rooms</c> list to change the <c>RoomType</c> type of.</param>
+        /// <param name="type">The <see cref="RoomType">RoomType</see> type to set the room to.</param>
+        /// <param name="index">The index of the room on the <see cref="rooms">rooms</see> list to change the <see cref="RoomType">RoomType</see> type of.</param>
         public void SetRoom(RoomType type, ushort index)
         {
-            rooms[index] = new Room(type, index);
+            rooms[index] = type;
         }
 
         /// <summary>
-        /// Gets the <c>RoomType</c> type of a room at <c>index</c> in the <c>rooms</c> list.
+        /// Gets the <see cref="RoomType">RoomType</see> type of a room at <c>index</c> in the <see cref="rooms">rooms</see> array.
         /// </summary>
-        /// <param name="index">The index of the room on the <c>rooms</c> list to check the type of.</param>
-        /// <returns>The <c>RoomType</c> type of room at the given location.</returns>
+        /// <param name="index">The index of the room on the <see cref="rooms">rooms</see> list to check the type of.</param>
+        /// <returns>The <see cref="RoomType">RoomType</see> type of room at the given location.</returns>
         /// <exception cref="InvalidOperationException"></exception>
         public RoomType GetRoomAt(int index)
         {
-            return rooms[index].type;
-        }
-
-        public RoomType GetRoomAtPos(int pos)
-        {
-            for (int i = 0; i < rooms.Count; i++)
-            {
-                if (rooms[i].pos == pos)
-                {
-                    return rooms[i].type;
-                }
-            }
-            throw new ArgumentException("There is no room with that position.");
-        }
-
-        public int GetIndexFromPos(int pos)
-        {
-
-        }
-
-        public int GetPosFromIndex()
-        {
-
-        }
-
-        /// <summary>
-        /// A type that stores a <c>RoomType</c> type and its position.
-        /// </summary>
-        private struct Room
-        {
-            public RoomType type { get; private set; }
-            public ushort pos { get; private set; }
-
-            public Room(RoomType type, ushort pos)
-            {
-                this.type = type;
-                this.pos = pos;
-            }
+            return rooms[index];
         }
     }
 }
