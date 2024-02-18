fbb3386e6705b9ac7041e6e04e1e3c315b01486b
havecookies <1059758@apps.nsd.org>
2024-02-18 14:59:00 -0800

    Documentation

--- Core/WumpusCore/GameLocations/GameLocations.cs
+++ Core/WumpusCore/GameLocations/GameLocations.cs
@@ -18,10 +18,13 @@ namespace WumpusCore.GameLocations
         }
 
         /// <summary>
-        /// The list of rooms and their positions.
+        /// The array of rooms.
         /// </summary>
         private RoomType[] rooms;
-
+        
+        /// <summary>
+        /// The array of rooms.
+        /// </summary>
         public RoomType[] Rooms
         {
             get { return rooms; }
@@ -62,7 +65,7 @@ namespace WumpusCore.GameLocations
         /// Sets the room at <c>index</c> to another <see cref="RoomType">RoomType</see> type.
         /// </summary>
         /// <param name="type">The <see cref="RoomType">RoomType</see> type to set the room to.</param>
-        /// <param name="index">The index of the room on the <see cref="rooms">rooms</see> list to change the <see cref="RoomType">RoomType</see> type of.</param>
+        /// <param name="index">The index of the room on the <see cref="rooms">rooms</see> array to change the <see cref="RoomType">RoomType</see> type of.</param>
         public void SetRoom(RoomType type, ushort index)
         {
             rooms[index] = type;
@@ -71,7 +74,7 @@ namespace WumpusCore.GameLocations
         /// <summary>
         /// Gets the <see cref="RoomType">RoomType</see> type of a room at <c>index</c> in the <see cref="rooms">rooms</see> array.
         /// </summary>
-        /// <param name="index">The index of the room on the <see cref="rooms">rooms</see> list to check the type of.</param>
+        /// <param name="index">The index of the room on the <see cref="rooms">rooms</see> array to check the type of.</param>
         /// <returns>The <see cref="RoomType">RoomType</see> type of room at the given location.</returns>
         public RoomType GetRoomAt(ushort index)
         {
