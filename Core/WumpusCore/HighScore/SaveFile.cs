bb2ad25f4aebe53bb727323645a5671965041352
ilhs1041495 <113475810+ilhs1041495@users.noreply.github.com>
2024-02-26 11:50:15 -0800

    save file things

--- Core/WumpusCore/HighScore/SaveFile.cs
+++ Core/WumpusCore/HighScore/SaveFile.cs
@@ -1,7 +1,30 @@
-﻿namespace WumpusCore.HighScoreNS
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace WumpusCore.HighScoreNS
 {
     internal class SaveFile
     {
+        public SaveFile()
+        {
+            // Create the file, or overwrite if the file exists.
+            using (FileStream fs = File.Create(path))
+            {
+                byte[] info = new UTF8Encoding(true).GetBytes("This is some text in the file.");
+                // Add some information to the file.
+                fs.Write(info, 0, info.Length);
+            }
 
+            // Open the stream and read it back.
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string s = "";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+        }
     }
 }
