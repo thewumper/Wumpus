a6c87a79af20a1e9427ff08cc9f4e72dc8920e3a
1118422ILHS <145387102+1118422ILHS@users.noreply.github.com>
2024-02-29 10:45:17 -0800

    Add Trivia minigame (#34)
    Adds a Trivia minigame, accessible through Trivia, with rounds that are won based on how many questions are correctly answered out of a set of N questions. Uses the Newtonsoft.json framework. Because Trivia reads its questions from a json file, creating an instance requires that the creator knows the path to the questions source file. This commit comes with 60 questions generated by ChatGPT, stored in Questions.json

---
+++ Core/WumpusCore/Trivia/AnsweredQuestion.cs
@@ -0,0 +1,42 @@
+using System;
+
+namespace WumpusCore.Trivia
+{
+    /// <summary>
+    /// A multiple choice question, containing question text, answer options, and the correct answer.
+    /// </summary>
+    public class AnsweredQuestion
+    {
+        public AnsweredQuestion(string questionText, string[] choices, int answer)
+        {
+            this.question = new AskableQuestion(questionText, choices);
+            this.answer = answer;
+        }
+
+        /// <summary>
+        /// Everything but the answer. Safe to hand out of Trivia.
+        /// </summary>
+        public readonly AskableQuestion question;
+        
+        /// <summary>
+        /// The text that will be presented as a question to the player.
+        /// </summary>
+        public string QuestionText
+        {
+            get { return question.questionText; }
+        }
+        
+        /// <summary>
+        /// Each choice of possible answer.
+        /// </summary>
+        public string[] choices
+        {
+            get { return question.choices; }
+        }
+        
+        /// <summary>
+        /// The index of the choice that is correct.
+        /// </summary>
+        public readonly int answer;
+    }
+}
