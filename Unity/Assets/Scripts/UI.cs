d2b2343719e9b0cbc33bf97a22711d511b3fc0bc
maybe.havecookies <1059758@apps.nsd.org>
2024-03-01 11:16:03 -0800

    Create UI.cs

---
+++ Unity/Assets/Scripts/UI.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WumpusCore.Controller;
+
+public class UI : MonoBehaviour
+{
+    private Controller controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = new Controller();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
