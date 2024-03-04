a51a88e81354e400ac0e75992ca6467ac01134cb
1118422 <145387102+1118422ILHS@users.noreply.github.com>
2024-03-04 11:25:18 -0800

    Remove exception cref

--- Core/WumpusCore/Entity/Entity.cs
+++ Core/WumpusCore/Entity/Entity.cs
@@ -34,7 +34,6 @@ namespace WumpusCore.Entity
         /// Gets all room indices adjacent to the room the Entity is currently in
         /// </summary>
         /// <returns>A list of integer indices of the adjacent rooms</returns>
-        /// <exception cref="NotImplementedException"></exception>
         public int[] GetAdjacentRooms()
         {
             throw new NotImplementedException();
@@ -45,7 +44,6 @@ namespace WumpusCore.Entity
         /// </summary>
         /// <param name="roomIndex">The room that might be adjacent</param>
         /// <returns>True if the given room is adjacent, False otherwise</returns>
-        /// <exception cref="NotImplementedException"></exception>
         public bool CheckIfAdjacent(int roomIndex)
         {
             throw new NotImplementedException();
@@ -55,7 +53,6 @@ namespace WumpusCore.Entity
         /// Tries to move to the given room, fails if not adjacent
         /// </summary>
         /// <param name="roomIndex">The room to move to</param>
-        /// <exception cref="NotImplementedException"></exception>
         protected void MoveToRoom(int roomIndex)
         {
             throw new NotImplementedException();
@@ -65,7 +62,6 @@ namespace WumpusCore.Entity
         /// Forces a move to the given room.
         /// </summary>
         /// <param name="roomIndex">The room index to move to</param>
-        /// <exception cref="NotImplementedException"></exception>
         protected void TeleportToRoom(int roomIndex)
         {
             throw new NotImplementedException();
@@ -74,7 +70,6 @@ namespace WumpusCore.Entity
         /// <summary>
         /// Moves once in a random direction.
         /// </summary>
-        /// <exception cref="NotImplementedException"></exception>
         protected void MoveToRandomAdjacent()
         {
             throw new NotImplementedException();
@@ -83,7 +78,6 @@ namespace WumpusCore.Entity
         /// <summary>
         /// Teleports to a random room on the map.
         /// </summary>
-        /// <exception cref="NotImplementedException"></exception>
         protected void TeleportToRandom()
         {
             throw new NotImplementedException();
@@ -93,7 +87,6 @@ namespace WumpusCore.Entity
         /// Returns True if the given Entity is in the same room as this Entity, False otherwise
         /// </summary>
         /// <param name="e">The entity that might be in the same room</param>
-        /// <exception cref="NotImplementedException"></exception>
         public bool CheckIfEntitySharingRoom(Entity e)
         {
             throw new NotImplementedException();
@@ -104,7 +97,6 @@ namespace WumpusCore.Entity
         /// </summary>
         /// <param name="e">The entity that might be adjacent to this Entity</param>
         /// <returns>True if the given Entity is in a room adjacent to the current room, False if otherwise, even if they are in the same room.</returns>
-        /// <exception cref="NotImplementedException"></exception>
         public bool CheckIfEntityAdjacent(Entity e)
         {
             throw new NotImplementedException();
@@ -115,7 +107,6 @@ namespace WumpusCore.Entity
         /// </summary>
         /// <param name="roomIndex">The room to navigate to</param>
         /// <returns>Distance (in accessible rooms) to the given room</returns>
-        /// <exception cref="NotImplementedException"></exception>
         public int AccessibleDistanceToRoom(int roomIndex)
         {
             throw new NotImplementedException();
@@ -126,7 +117,6 @@ namespace WumpusCore.Entity
         /// </summary>
         /// <param name="e">The Entity to navigate to</param>
         /// <returns>Distance (in accessible rooms) to the given Entity</returns>
-        /// <exception cref="NotImplementedException"></exception>
         public int AccessibleDistanceToEntity(Entity e)
         {
             throw new NotImplementedException();
@@ -137,7 +127,6 @@ namespace WumpusCore.Entity
         /// </summary>
         /// <param name="roomIndex">The room to find the distance to</param>
         /// <returns>The distance to the given room</returns>
-        /// <exception cref="NotImplementedException"></exception>
         public int DistanceToRoom(int roomIndex)
         {
             throw new NotImplementedException();
@@ -148,7 +137,6 @@ namespace WumpusCore.Entity
         /// </summary>
         /// <param name="e">The Entity to find the distance to</param>
         /// <returns>The distance to the given Entity</returns>
-        /// <exception cref="NotImplementedException"></exception>
         public int DistanceToEntity(Entity e)
         {
             throw new NotImplementedException();
