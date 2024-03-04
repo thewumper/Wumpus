4ea63daad046796640c74e81dd8f16bd2c0aab6a
1118422ILHS <145387102+1118422ILHS@users.noreply.github.com>
2024-03-04 11:34:26 -0800

    Stub Entity (#37)
    Stub for Entity class and corresponding testing.
    
    Entity is a basic class to be extended by Player, Wumpus, and Lucky Cat. It controls location and provides methods to move itself and examine its surroundings.

---
+++ Core/WumpusTesting/EntityTest.cs
@@ -0,0 +1,75 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WumpusCore.Entity;
+
+namespace WumpusTesting
+{
+    [TestClass]
+    public class EntityTest: Entity
+    {
+        [TestMethod]
+        public void TestGetAdjacentRooms()
+        {
+            //
+        }
+        
+        [TestMethod]
+        public void TestMoveToRoom()
+        {
+            //
+        }
+        
+        [TestMethod]
+        public void TestTeleportToRoom()
+        {
+            //
+        }
+        
+        [TestMethod]
+        public void TestGetLocaton()
+        {
+            //
+        }
+        
+        [TestMethod]
+        public void TestTeleportToRandom()
+        {
+            //
+        }
+        
+        [TestMethod]
+        public void TestCheckIfEntitySharingRoom()
+        {
+            //
+        }
+        
+        [TestMethod]
+        public void TestCheckIfEntityAdjacent()
+        {
+            //
+        }
+        
+        [TestMethod]
+        public void TestAccessibleDistanceToRoom()
+        {
+            //
+        }
+        
+        [TestMethod]
+        public void TestAccessibleDistanceToEntity()
+        {
+            //
+        }
+        
+        [TestMethod]
+        public void TestDistanceToRoom()
+        {
+            //
+        }
+        
+        [TestMethod]
+        public void TestDistanceToEntity()
+        {
+            //
+        }
+    }
+}
