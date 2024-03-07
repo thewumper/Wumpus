db63d2433da3609284a09317ecde1322e115aaea
maybe.havecookies <1059758@apps.nsd.org>
2024-03-07 11:17:56 -0800

    uuuuuuuuuuugggggggggggggghhhhhhhhhhhhhhhhhhhh

--- Unity/Assets/Scripts/UI.cs
+++ Unity/Assets/Scripts/UI.cs
@@ -10,6 +10,20 @@ public class UI : MonoBehaviour
     private GameObject cam;
 
     private const float camSens = 5f;
+    
+    // doors
+    [SerializeField]
+    private GameObject northDoor;
+    [SerializeField]
+    private GameObject northEastDoor;
+    [SerializeField]
+    private GameObject southEastDoor;
+    [SerializeField]
+    private GameObject southDoor;
+    [SerializeField]
+    private GameObject southWestDoor;
+    [SerializeField]
+    private GameObject northWestDoor;
 
     // Start is called before the first frame update
     void Start()
@@ -21,9 +35,31 @@ public class UI : MonoBehaviour
     void FixedUpdate()
     {
         IRoom room = controller.GetRoom(0);
-        for (int i = 0; i < room.ExitRooms.Count; i++)
+        foreach (Directions dir in room.ExitDirections)
         {
-            //room.ExitRooms[];
+            string name = DirectionHelper.GetShortNameFromDirection(dir);
+            Debug.Log(name);
+            switch (name)
+            {
+                case "N":
+                    northDoor.SetActive(true);
+                    break;
+                case "NE":
+                    northEastDoor.SetActive(true);
+                    break;
+                case "SE":
+                    southEastDoor.SetActive(true);
+                    break;
+                case "S":
+                    southDoor.SetActive(true);
+                    break;
+                case "SW":
+                    southWestDoor.SetActive(true);
+                    break;
+                case "NW":
+                    northWestDoor.SetActive(true);
+                    break;
+            }
         }
     }
 
