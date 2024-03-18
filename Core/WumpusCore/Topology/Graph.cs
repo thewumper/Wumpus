7d4eabc5713338fdefe3878dc1be559f34b47db0
RogueFirework <70032201+roguefirework@users.noreply.github.com>
2024-03-18 12:03:08 -0700

    Add first graph function and a basic unit test

---
+++ Core/WumpusCore/Topology/Graph.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace WumpusCore.Topology
+{
+    public class Graph
+    {
+        private List<IRoom> nodes;
+
+        public Graph(List<IRoom> nodes)
+        {
+            this.nodes = nodes;
+        }
+        
+        /// <summary>
+        /// Checks if removing a node disconnects graph
+        /// </summary>
+        /// <param name="room">Node to remove</param>>
+        /// <returns>True if the node can be removed without disconnecting the graph</returns>
+        public bool IsNodeRemovalValid(IRoom room)
+        {
+            // If we are to small it just doesn't make any sense
+            if (nodes.Count <= 1)
+            {
+                return false;
+            }
+            HashSet<IRoom> visited = new HashSet<IRoom> { room };
+            Queue<IRoom> toExplore = new Queue<IRoom>();
+            toExplore.Enqueue(nodes[0] != room ? nodes[0] : nodes[1]);
+            while (toExplore.Count > 0)
+            {
+                var currentRoom = toExplore.Dequeue();
+                if (visited.Contains(currentRoom))
+                {
+                    continue;
+                }
+                visited.Add(currentRoom);
+                foreach (var exit in currentRoom.ExitRooms.Values)
+                {
+                    if (!(visited.Contains(exit) || toExplore.Contains(exit)))
+                    {
+                        toExplore.Enqueue(exit);
+                    }
+                }
+            }
+            return visited.Count == nodes.Count;
+        }
+    }
+}
---
+++ Core/WumpusTesting/GraphTests.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WumpusCore.Topology;
+
+namespace WumpusTesting
+{       
+    [TestClass] 
+    public class GraphTests
+    {
+        GraphTests()
+        {
+            
+        }
+
+        [TestMethod]
+        public void TestSimpleConfig()
+        {
+            List<IRoom> nodes = new List<IRoom>
+            {
+                new TestNode(),
+                new TestNode(),
+                new TestNode()
+            };
+            ((TestNode)nodes[0]).Connect(nodes[1], Directions.North).Connect(nodes[2],Directions.North);
+            Graph graph = new Graph(nodes);
+            Assert.IsTrue(graph.IsNodeRemovalValid(nodes[0]));
+            Assert.IsFalse(graph.IsNodeRemovalValid(nodes[1]));
+            Assert.IsTrue(graph.IsNodeRemovalValid(nodes[2]));
+        }
+        
+    }
+    public class TestNode : IRoom 
+    {
+
+
+        public TestNode Connect(IRoom other,Directions direction)
+        {
+            this.ExitRooms[direction] = other;
+            other.ExitRooms[direction.GetInverse()] = this;
+            return (TestNode)other;
+        }
+        
+        public Directions[] ExitDirections => ExitRooms.Keys.ToArray();
+
+        public Dictionary<Directions, IRoom> ExitRooms { get; set; }
+        
+        public ushort Id => 0;
+    }
+    
+}
