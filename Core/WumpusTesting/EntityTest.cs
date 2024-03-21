f986a5bb5b92b69e84bba8afd244a97387d512af
1118422 <145387102+1118422ILHS@users.noreply.github.com>
2024-03-21 11:14:01 -0700

    Fix tests to match reality

--- Core/WumpusTesting/EntityTest.cs
+++ Core/WumpusTesting/EntityTest.cs
@@ -17,6 +17,16 @@ namespace WumpusTesting
             {
                 for (int i = 0; i < 30; i++)
                 {
+                    // All doors open
+                    outputFile.WriteLine("N,NE,SE,S,SW,NW");
+                }
+            }
+            
+            // Write the string array to a new file named "WriteLines.txt".
+            using (StreamWriter outputFile = new StreamWriter("entitytest2.map"))
+            {
+                for (int i = 0; i < 30; i++)
+                {
                     // No traveling south-west
                     outputFile.WriteLine("N,NE,SE,S,NW");
                 }
@@ -26,6 +36,15 @@ namespace WumpusTesting
         public GameLocations SetupGameLocations()
         {
             Topology topology = new Topology("entitytest1.map");
+            GameLocations game = new GameLocations(30);
+            game.AddEntity(new DummyEntity(topology, game, EntityType.Player));
+            game.AddEntity(new Entity(topology, game, 20, EntityType.Wumpus));
+            return game;
+        }
+        
+        public GameLocations SetupGameLocationsDirected()
+        {
+            Topology topology = new Topology("entitytest2.map");
             GameLocations game = new GameLocations(30);
             game.AddEntity(new DummyEntity(topology, game, EntityType.Player));
             game.AddEntity(new Entity(topology, game, 20, EntityType.Wumpus));
@@ -41,7 +60,42 @@ namespace WumpusTesting
         public void TestGetAdjacentRooms()
         {
             GameLocations game = SetupGameLocations();
-            Assert.AreEqual(new ushort[] { 9, 8, 14, 21, 16, 10 }, getEntity(game).GetAdjacentRooms());
+            // Clockwise from top
+            ushort[] expected = new ushort[] { 9, 10, 16, 21, 14, 8 };
+            ushort[] actual = getEntity(game).GetAdjacentRooms();
+            Assert.AreEqual(expected[0], actual[0]);
+            Assert.AreEqual(expected[1], actual[1]);
+            Assert.AreEqual(expected[2], actual[2]);
+            Assert.AreEqual(expected[3], actual[3]);
+            Assert.AreEqual(expected[4], actual[4]);
+            Assert.AreEqual(expected[5], actual[5]);
+            Assert.AreEqual(6, actual.Length);
+        }
+        
+        [TestMethod]
+        public void TestGetAccessibleRooms()
+        {
+            GameLocations game = SetupGameLocations();
+            // Clockwise from top
+            ushort[] expected = new ushort[] { 9, 10, 16, 21, 14, 8 };
+            ushort[] actual = getEntity(game).GetAccessibleRooms();
+            Assert.AreEqual(expected[0], actual[0]);
+            Assert.AreEqual(expected[1], actual[1]);
+            Assert.AreEqual(expected[2], actual[2]);
+            Assert.AreEqual(expected[3], actual[3]);
+            Assert.AreEqual(expected[4], actual[4]);
+            Assert.AreEqual(expected[5], actual[5]);
+            Assert.AreEqual(6, actual.Length);
+            
+            game = SetupGameLocationsDirected();
+            expected = new ushort[] { 9, 10, 16, 21, 8 };
+            actual = getEntity(game).GetAccessibleRooms();
+            Assert.AreEqual(expected[0], actual[0]);
+            Assert.AreEqual(expected[1], actual[1]);
+            Assert.AreEqual(expected[2], actual[2]);
+            Assert.AreEqual(expected[3], actual[3]);
+            Assert.AreEqual(expected[4], actual[4]);
+            Assert.AreEqual(5, actual.Length);
         }
         
         [TestMethod]
@@ -102,17 +156,17 @@ namespace WumpusTesting
         public void TestCheckIfEntityAdjacent()
         {
             GameLocations game = SetupGameLocations();
-            Assert.IsFalse(getEntity(game).CheckIfEntitySharingRoom(game.GetEntity(EntityType.Wumpus)));
+            Assert.IsFalse(getEntity(game).CheckIfEntityAdjacent(game.GetEntity(EntityType.Wumpus)));
             getEntity(game).MoveToRoomWrapper(14);
-            Assert.IsTrue(getEntity(game).CheckIfEntitySharingRoom(game.GetEntity(EntityType.Wumpus)));
+            Assert.IsTrue(getEntity(game).CheckIfEntityAdjacent(game.GetEntity(EntityType.Wumpus)));
             getEntity(game).MoveToRoomWrapper(20);
-            Assert.IsFalse(getEntity(game).CheckIfEntitySharingRoom(game.GetEntity(EntityType.Wumpus)));
+            Assert.IsFalse(getEntity(game).CheckIfEntityAdjacent(game.GetEntity(EntityType.Wumpus)));
         }
         
         [TestMethod]
         public void TestAccessibleDistanceToEntity()
         {
-            GameLocations game = SetupGameLocations();
+            GameLocations game = SetupGameLocationsDirected();
             Assert.AreEqual(3, getEntity(game).AccessibleDistanceToEntity(game.GetEntity(EntityType.Wumpus)));
             getEntity(game).TeleportToRoomWrapper(29);
             Assert.AreEqual(3, getEntity(game).AccessibleDistanceToEntity(game.GetEntity(EntityType.Wumpus)));
@@ -129,7 +183,7 @@ namespace WumpusTesting
         [TestMethod]
         public void TestDistanceToEntity()
         {
-            GameLocations game = SetupGameLocations();
+            GameLocations game = SetupGameLocationsDirected();
             Assert.AreEqual(2, getEntity(game).DistanceToEntity(game.GetEntity(EntityType.Wumpus)));
             getEntity(game).TeleportToRoomWrapper(29);
             Assert.AreEqual(3, getEntity(game).DistanceToEntity(game.GetEntity(EntityType.Wumpus)));
