a869672808b75dddfa9e089fb34c81ab1ad527d6
Odin Hall <1115300@apps.nsd.org>
2024-03-28 10:30:55 -0700

    Cat Kitty Cat Cat Kitty Cat Cat

--- Core/WumpusCore/LuckyCat/Cat.cs
+++ Core/WumpusCore/LuckyCat/Cat.cs
@@ -23,12 +23,16 @@ namespace WumpusCore.LuckyCat
         /// <summary>
         /// The radius that the cat should be heard from
         /// </summary>
-        public const int AudibleMewingRadius = 2;
+        public const int AudibleMewingRadius = 1;
 
         /// <summary>
         /// Location of cat entity
         /// </summary>
         public int Location;
+
+        public Cat(Topology.Topology topology, GameLocations.GameLocations parent, ushort location, EntityType entityType) : base(topology, parent, location, entityType)
+        {
+        }
 
         /// <summary>
         /// Attempts to Tame the Lucky Cat
@@ -67,7 +71,7 @@ namespace WumpusCore.LuckyCat
         {
             // (You pet the cat, yippee)
         }
-        
+
         /// <summary>
         /// Plays the mewing audio file if player is nearby the cat
         /// Will depend on audio manager for sound
@@ -75,21 +79,20 @@ namespace WumpusCore.LuckyCat
         /// <returns> Audio id </returns>
         /// <exception cref="NotImplementedException"></exception>
         public int Mew()
-        {
-/*          
-            if (DistanceToEntity() < AudibleMewingRadius) 
+        {          
+
+            if (AccessibleDistanceToEntity(gameLocations.GetEntity(EntityType.Player)) < AudibleMewingRadius)
             {
                 return 69;
             }
             
-            else if (DistanceToEntity() == AudibleMewingRadius)
+            else if (AccessibleDistanceToEntity(gameLocations.GetEntity(EntityType.Player)) == AudibleMewingRadius)
             {
                 return 70;
             }
 
-            else { return 0; }
-*/ 
-            throw new NotImplementedException();
+            else { return 0; } 
+
         }
     }
 }
