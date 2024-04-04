4facb9218f9c8011466e8ed8645ad2529fb82e68
ilhs1041495 <113475810+ilhs1041495@users.noreply.github.com>
2024-04-04 10:05:20 -0700

    High score (#56)
    * Basic implementation of four methods
    
    Object constructor stores fields, calculate score() uses the equation from spec, translate wumpus life() turns the bool into an int for equation, compare high score() checks the score field and current high score with Math.Max
    
    * Added save file file
    
    * Changed namespace name
    
    * Working on tests
    
    * I have massacred my code :)
    
    * Basic Tests
    
    Test for score calculation, constructor for high Score object, and stored high score struct
    
    * save file things
    
    * SaveFolder and first test
    
    it failed the test (access denied, unauthorizedAccessException)
    
    * Got files working and worked on top 10 high score list
    
    * topTenHighScores sorting
    
    * StoredHighScore back to string progress
    
    * SaveFile Methods, started separating top scores
    
    * StoredHighScore got from file
    
    almost ready to put them back into the top ten for the other retrieves
    
    * Made random score method for test
    
    * Fixed random test, tried to load topTenScores
    
    * bool for text printing
    
    Might have a solution for the file problem
    
    * New constructors for save file and high score objects
    
    * It finally saves!
    
    * Comments and documentation
    
    might still need to clean some code, but this is basically finished now
    
    * actually testing some tests
    
    was using a few of the tests as just running for output, now actually testing them, but not done with it
    
    * :ant: fixed tests and function names
    
    :ant:
    
    * Add back EntityType (oops)
    
    :aerial_tramway:
    
    ---------
    
    Co-authored-by: Jayder <113476107+ilhs1096069@users.noreply.github.com>

---
+++ Core/WumpusCore/HighScore/SaveFile.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace WumpusCore.HighScoreNS
+{
+    internal class SaveFile
+    {
+        /// <summary>
+        /// The file path used by the savefile object
+        /// </summary>
+        public readonly string path;
+
+        /// <summary>
+        /// Creates a save file at a set directory
+        /// </summary>
+        /// <param name="text"> What information to save to the file </param>
+        /// <param name="readText"> Whether or not to print the text to console </param>
+        public SaveFile(string text, bool readText)
+        {
+            string appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            string wumpusSaveDirectory = Path.Combine(appDataPath, "WumpusGame");
+            if (!Directory.Exists(wumpusSaveDirectory))
+            {
+                Directory.CreateDirectory(wumpusSaveDirectory);
+            }
+
+            this.path = wumpusSaveDirectory;
+
+            // Create the file, or overwrite if the file exists.
+            CreateFile(text);
+
+            // Open the stream and read it back.
+            ReadFile(readText);
+        }
+
+        /// <summary>
+        /// Takes a directory to create a file
+        /// </summary>
+        /// <param name="readText"> file path to get information from </param>
+        /// <param name="readText"> Whether or not to print the text to console </param>
+        public SaveFile(bool readText, string path)
+        {
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            this.path = path;
+
+            ReadFile(readText);
+        }
+
+        /// <summary>
+        /// Uses the directory to create a file
+        /// with the given text stored
+        /// </summary>
+        /// <param name="text"> what text to store in the file </param>
+        public void CreateFile(string text)
+        {
+            using (FileStream fs = File.Create(Path.Combine(this.path, "SaveData.txt")))
+            {
+                byte[] info = new UTF8Encoding(true).GetBytes(text);
+                // Add some information to the file.
+                fs.Write(info, 0, info.Length);
+            }
+        }
+
+        /// <summary>
+        /// Reads the information on the file
+        /// </summary>
+        /// <param name="printText"> whether or not to print the text from the file </param>
+        /// <returns> string information of file </returns>
+        public string ReadFile(bool printText)
+        {
+            using (StreamReader sr = File.OpenText(Path.Combine(this.path, "SaveData.txt")))
+            {
+                string s = "";
+                string fullText = "";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    fullText += s;
+                    if (printText) { Console.WriteLine(s); }
+                }
+                return fullText;
+            }
+        }
+    }
+}
