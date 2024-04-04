e0391150e25cf65588a3647f6327098aeace164d
ILHS2000503 <2000503@apps.nsd.org>
2024-04-04 10:59:48 -0700

    Now with entity(I think)

--- Core/WumpusCore/Wumpus/Wumpus.cs
+++ Core/WumpusCore/Wumpus/Wumpus.cs
@@ -1,55 +1,52 @@
-﻿namespace WumpusCore.Wumpus
+﻿using System;
+using WumpusCore.Entity;
+
+namespace WumpusCore.Wumpus
 {
         /// <summary>
         /// state of wumpus.  
         /// Better description coming soon
         /// </summary>
         public enum State
-        {
+        { 
             Sleeping,
             FleeingArrow,
             FleeingCombat,
             Combat,
             Dead
         }
-    internal class Wumpus
+    public class Wumpus: Entity.Entity
     {
-        /// <summary>
-        /// Current Room Wumpus is in.  
-        /// Better description coming soon
-        /// </summary>
-        public int Position { get; private set; }
         private State WumpusState;
         /// <summary>
         /// Constructs the Wumpus.  
-        /// Better description coming soon
         /// </summary>
-        public Wumpus()
+        public Wumpus(Topology.Topology topology, GameLocations.GameLocations parent, ushort location, EntityType entityType) : base(topology, parent, location, entityType)
         {
-            Position = 0;
             WumpusState = State.Sleeping;
         }
         /// <summary>
-        /// Moves the wumpus into a connected room randomly a number of times depending on the state.  
-        /// Better description coming soon
+        /// Moves the wumpus into a connected room randomly a number of times depending on the state.
         /// </summary>
         /// <param name="Random">Represents a pseudo-random number generator, which is a device that produces a sequence of numbers that meet certain statistical requirements for randomness</param>
-        /// <exception cref="NotImplementedException"></exception>
         public void move(Random Random)
         {
-            throw new NotImplementedException();
             int maxMoves;
-            if (WumpusState = 1)
+            if ((int)WumpusState == 1)
             {
                 maxMoves = 1;
             }
-            else if (WumpusState = 2)
+            else if ((int)WumpusState == 2)
             {
                 maxMoves = 4;
             }
+            else
+            {
+                maxMoves = 0;
+            }
                 for (int i = 0; i<maxMoves; i++){
-                if ((maxMoves==4&&i <= 1)||rand.Next(0,2)==1) {
-                //code to move
+                if ((maxMoves==4&&i <= 1)||Random.Next(0,2)==1) {
+                    MoveToRandomAdjacent();
                 }
             }
             WumpusState = State.Sleeping;
