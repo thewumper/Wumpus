143d630dd6f2f70a8be04dac849606dbb05bb8a5
Jayder <113476107+ilhs1096069@users.noreply.github.com>
2024-04-15 11:51:40 -0700

    Add StopSound

--- Unity/Assets/Scripts/SoundManager.cs
+++ Unity/Assets/Scripts/SoundManager.cs
@@ -6,6 +6,9 @@ using WumpusCore.Topology;
 public class SoundManager
 {
     private AudioClip wumpusClip;
+    private AudioClip luckyCatClip;
+    private AudioClip batsClip;
+    private AudioClip ratsClip;
 
     public SoundManager(AudioClip wumpusClip)
     {
@@ -27,19 +30,26 @@ public class SoundManager
     /// <param name="door">The door to play the sound</param>
     public void PlaySound(SoundType type, GameObject door)
     {
+        if (door == null)
+        {
+            throw new InvalidOperationException("Door is null.");
+        }
+
+        // Get the component or create one if it doesn't exist.
         AudioSource doorAudioSource = door.GetComponent<AudioSource>();
         if (door.GetComponent<AudioSource>() == null)
         {
             doorAudioSource = door.AddComponent<AudioSource>();
         }
+        
+        // Set the sound clip
+        doorAudioSource.clip = GetAudioClipFromType(type);
 
-        if (type == SoundType.Wumpus)
-        {
-            doorAudioSource.clip = wumpusClip;
-            doorAudioSource.loop = true;
-            doorAudioSource.spatialBlend = 1.0f;
-        }
+        // Set a few settings
+        doorAudioSource.loop = true;
+        doorAudioSource.spatialBlend = 1.0f;
 
+        // Play!
         doorAudioSource.Play();
     }
 
@@ -56,10 +66,43 @@ public class SoundManager
         }
     }
 
+    /// <summary>
+    /// Stops playing the sound on the specified door
+    /// </summary>
+    /// <param name="door">The door to stop playing the sound on</param>
     public void StopSound(GameObject door)
     {
+        if (door == null)
+        {
+            throw new InvalidOperationException("Door is null.");
+        }
+        
+        AudioSource doorAudioSource = door.GetComponent<AudioSource>();
+        if (doorAudioSource == null)
+        {
+            throw new InvalidOperationException("Audio Source does not exist on door.");
+        }
 
+        // Stops the sound by making the audio clip null and stopping the sound
+        doorAudioSource.Stop();
+        doorAudioSource.clip = null;
     }
 
+    /// <summary>
+    /// Gets an audio clip from the given sound type
+    /// </summary>
+    /// <param name="type">The given sound type</param>
+    /// <returns>An audio clip</returns>
+    /// <exception cref="InvalidOperationException"></exception>
+    private AudioClip GetAudioClipFromType(SoundType type)
+    {
+        switch (type)
+        {
+            case SoundType.Wumpus:
+                return wumpusClip;
+            default:
+                throw new InvalidOperationException("Given sound type does not have a sound clip associated with it.");
+        }
+    }
 }
 
