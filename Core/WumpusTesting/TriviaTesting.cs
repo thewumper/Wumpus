dbe3568fa41d35902b135484706d76fc13b37d9c
1118422 <145387102+1118422ILHS@users.noreply.github.com>
2024-05-02 10:26:08 -0700

    Change trivia file path

--- Core/WumpusTesting/TriviaTesting.cs
+++ Core/WumpusTesting/TriviaTesting.cs
@@ -10,7 +10,7 @@ namespace WumpusTesting
     {
         public Trivia makeTrivia()
         {
-            string path = Path.GetFullPath("..\\..\\..\\WumpusCore\\Trivia\\Questions.json");
+            string path = Path.GetFullPath("..\\..\\..\\..\\Unity\\Assets\\Trivia\\Questions.json");
             Trivia trivia = new Trivia(path);
             if (trivia.Count == 0)
             {
