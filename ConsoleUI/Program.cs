c5384fa94b822e191d6210c5429a356bc53099da
SilasMetting <93603994+SilasMetting@users.noreply.github.com>
2024-05-07 14:34:02 -0700

    working

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,13 +1,19 @@
-﻿using WumpusCore.Controller;
+﻿using System.Data.Common;
+using System.Security.Cryptography.X509Certificates;
+using WumpusCore.Controller;
+using WumpusCore.Topology;
+using WumpusCore.Trivia;
 
 namespace ConsoleUI
 {
-    public class ConsoleUI
+    public class Program
     {
         public static void Main(string[] args)
         {
-            Controller controller = new Controller("../../../../Unity/Assets/Trivia/Questions.json", "../../../../Unity/Assets/Maps", 0);
-            Console.WriteLine(controller.GetState());
+            Controller controller = new Controller("../../../Questions.json", "../../../", 0);
+            ConsoleUI ui = new ConsoleUI();
+            ui.StartGame(controller);
         }
+
     }
 }
