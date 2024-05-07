636da306019bb0ca64dbd6cba406814b9175a124
1118422 <145387102+1118422ILHS@users.noreply.github.com>
2024-05-07 14:43:12 -0700

    Add doc, abstract display ID, clean up csproj

--- Core/WumpusCore/Battle/BulletHell/Bullet.cs
+++ Core/WumpusCore/Battle/BulletHell/Bullet.cs
@@ -3,9 +3,12 @@ namespace WumpusCore.Battle.BulletHell
     /// <summary>
     /// A hazardous object in 2d space
     /// </summary>
-    public class Bullet
+    public class Bullet: IDisplayable
     {
-        public readonly int DisplayID;
+        /// <summary>
+        /// Represents the type of bullet
+        /// </summary>
+        public int DisplayID { get; }
         
         /// <summary>
         /// The bullet's position in space
@@ -25,9 +28,23 @@ namespace WumpusCore.Battle.BulletHell
         /// <summary>
         /// The amount of health that impact with this bullet reduces from the player
         /// </summary>
-        public int Damage;
+        public int Damage { get; private set; }
+        
+        /// <summary>
+        /// The size of the bullet
+        /// </summary>
+        public double Size { get; private set; }
 
-        public Bullet(Vector2 position, Vector2 velocity, Vector2 acceleration, int damage, int displayID)
+        /// <summary>
+        /// Constructs a Bullet
+        /// </summary>
+        /// <param name="position">The position of the Bullet</param>
+        /// <param name="velocity">The speed and direction that the Bullet moves in</param>
+        /// <param name="acceleration">The magnitude and direction of the Bullet's acceleration. Recommended to be 0.</param>
+        /// <param name="damage">The amount of damage the Bullet inflicts upon contact with the player</param>
+        /// <param name="displayID">A number representing the sprite of the bullet, as well as any effects it may have</param>
+        /// <param name="size">The size of the bullet. Changes at what point the bullet connects with the player.</param>
+        public Bullet(Vector2 position, Vector2 velocity, Vector2 acceleration, int damage, int displayID, double size)
         {
             Position = position;
             Velocity = velocity;
@@ -44,6 +61,7 @@ namespace WumpusCore.Battle.BulletHell
         {
             Position.Increment(Velocity);
             Velocity.Increment(Acceleration);
+            
         }
     }
 }
