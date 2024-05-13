bd82a562346651b4e63049a6c246d15112e57c96
RogueFirework <70032201+roguefirework@users.noreply.github.com>
2024-05-13 11:52:40 -0700

    Partially complete shaders

---
+++ Unity/Assets/Scripts/ShaderApplication.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+
+namespace WumpusUnity
+{
+    public class ShaderApplication : MonoBehaviour
+    {
+        [SerializeField]
+        private int width;
+        [SerializeField]
+        private int height;
+        [SerializeField]
+        private Shader shader;
+        private Material material;
+        private static readonly int Width = Shader.PropertyToID("_width");
+        private static readonly int Height = Shader.PropertyToID("_height");
+
+        public void Awake()
+        {
+            
+            material = new Material(shader);
+
+        }
+
+        public void OnRenderImage(RenderTexture source, RenderTexture destination)
+        {
+            material.SetInteger(Width,width);
+            material.SetInteger(Height,width);            
+            Graphics.Blit(source,destination,material);
+        }
+    }
+}
