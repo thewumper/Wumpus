17889d0817a04ceaa1ef238aa0f2dc7eae205611
1118422 <145387102+1118422ILHS@users.noreply.github.com>
2024-05-13 11:23:54 -0700

    Abstract to IEntity

--- Core/WumpusCore/Battle/BulletHell/ISpawner.cs
+++ Core/WumpusCore/Battle/BulletHell/ISpawner.cs
@@ -1,22 +1,7 @@
 namespace WumpusCore.Battle.BulletHell
 {
-    public interface ISpawner: IDisplayable
+    public interface ISpawner: IEntity
     {
-        /// <summary>
-        /// The spawner's position in space
-        /// </summary>
-        Vector2 Position { get; }
-        
-        /// <summary>
-        /// The direction the spawner is moving in space
-        /// </summary>
-        Vector2 Velocity { get; }
-
-        /// <summary>
-        /// The direction the spawner is curving in its motion
-        /// </summary>
-        Vector2 Acceleration { get; }
-
         /// <summary>
         /// Advances the spawner in time
         /// </summary>
