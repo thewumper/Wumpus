07518fee0799a9732bb2a720895187c5e57f38c1
1118422ILHS <1118422@apps.nsd.org>
2024-05-21 00:12:15 -0700

    Complete basic player physics

--- Unity/Assets/Scripts/Battle/BattlePlayerController.cs
+++ Unity/Assets/Scripts/Battle/BattlePlayerController.cs
@@ -1,68 +1,81 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class PlayerController : MonoBehaviour
 {
-    [FormerlySerializedAs("rb")] [SerializeField] private Rigidbody2D rigidbody;
-    [Range(0f, 1f)] [SerializeField] private float m_AccelerationFalloff = .8f;
-    [Range(0f, 1f)] [SerializeField] private float m_VelocityFalloff = .9f;
-    [Range(-100f, 100f)] [SerializeField] private float m_AccelerationMax = 10f;
-    [SerializeField] private LayerMask m_RoomCollider;
-    [Range(0f, 10f)] [SerializeField] private float speed = 1.0f;
-    private Vector2 m_Acceleration;
+    [SerializeField] private new Rigidbody2D rigidbody;
+    [Range(0f, 1f)] [SerializeField] private float accelerationFalloff = .95f;
+    [Range(0f, 1f)] [SerializeField] private float velocityFalloff = .98f;
+    [Range(0f, 1f)] [SerializeField] private float rotationFalloff = .9f;
+    [Range(0f, 0.1f)] [SerializeField] private float rotationUpdateCutoff = .01f;
+    [Range(-100f, 100f)] [SerializeField] private float accelerationMax = 100f;
+    [Range(0f, 1f)] [SerializeField] private float shake = 0.001f;
+    [SerializeField] private LayerMask roomCollider;
+    [Range(0f, 50f)] [SerializeField] private float speed = 8.0f;
+    private Vector2 acceleration;
+    private Vector2 orientation;
 
     private void Awake()
     {
         rigidbody.velocity = Vector2.zero;
-        m_Acceleration = Vector2.zero;
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+        acceleration = Vector2.zero;
+        orientation = Vector2.up;
+        rigidbody.inertia = float.MaxValue;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Normalize input and scale by dT
         Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         input.Normalize();
         input *= speed * Time.deltaTime;
+        
         // Acceleration slows over time
-        m_Acceleration += input;
-        m_Acceleration *= m_AccelerationFalloff;
+        acceleration *= accelerationFalloff;
+        
+        // Take in input
+        acceleration += input;
+        
+        // Hard cap on acceleration
+        if (acceleration.x > accelerationMax)
+        {
+            acceleration.x = accelerationMax;
+        }
+        if (acceleration.y > accelerationMax)
+        {
+            acceleration.y = accelerationMax;
+        }
+        if (acceleration.x < -accelerationMax)
+        {
+            acceleration.x = -accelerationMax;
+        }
+        if (acceleration.y < -accelerationMax)
+        {
+            acceleration.y = -accelerationMax;
+        }
         
         // Velocity follows acceleration, with simple drag
-        rigidbody.velocity += m_Acceleration;
-        rigidbody.velocity *= m_VelocityFalloff;
-        //transform.Translate(rigidbody.velocity);
-    }
-    
-    public void Move(float moveX, float moveY)
-    {
-        // Simply add more acceleration
-        m_Acceleration += new Vector2(moveX, moveY);
+        rigidbody.velocity += acceleration;
+        rigidbody.velocity *= velocityFalloff;
         
-        // Hard cap
-        if (m_Acceleration.x > m_AccelerationMax)
+        // Update rotation
+        Vector2 currentDirection = rigidbody.velocity.normalized;
+        if (rigidbody.velocity.magnitude > rotationUpdateCutoff)
         {
-            m_Acceleration.x = m_AccelerationMax;
+            orientation += UnityEngine.Random.insideUnitCircle.normalized * shake;
+            orientation.Normalize();
+            orientation *= rotationFalloff;
+            orientation += currentDirection * (1 - rotationFalloff);
+            orientation.Normalize();
         }
-        if (m_Acceleration.y > m_AccelerationMax)
+
+        if (orientation.magnitude == 0)
         {
-            m_Acceleration.y = m_AccelerationMax;
+            orientation = Vector2.up;
         }
-        if (m_Acceleration.x < -m_AccelerationMax)
-        {
-            m_Acceleration.x = -m_AccelerationMax;
-        }
-        if (m_Acceleration.y < -m_AccelerationMax)
-        {
-            m_Acceleration.y = -m_AccelerationMax;
-        }
+        
+        // Set rotation
+        rigidbody.rotation = (float)(180.0 * Math.Atan2(-orientation.x, orientation.y) / Math.PI);
     }
 }
