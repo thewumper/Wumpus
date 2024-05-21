3d456af7de65524f790e25a75b4b67d7edf5f3c6
1118422ILHS <145387102+1118422ILHS@users.noreply.github.com>
2024-05-21 01:27:50 -0700

    Basic player control in battle

--- Unity/Assets/Scripts/Battle/BattlePlayerController.cs
+++ Unity/Assets/Scripts/Battle/BattlePlayerController.cs
@@ -4,13 +4,37 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private new Rigidbody2D rigidbody;
+    /// <summary>
+    /// Higher values smooth out acceleration more
+    /// </summary>
     [Range(0f, 1f)] [SerializeField] private float accelerationFalloff = .95f;
+    /// <summary>
+    /// Higher values mean more slippery floors
+    /// </summary>
     [Range(0f, 1f)] [SerializeField] private float velocityFalloff = .98f;
+    /// <summary>
+    /// Higher values mean slower turning
+    /// </summary>
     [Range(0f, 1f)] [SerializeField] private float rotationFalloff = .9f;
+    /// <summary>
+    /// The minimum velocity at which rotation continues to update
+    /// </summary>
     [Range(0f, 0.1f)] [SerializeField] private float rotationUpdateCutoff = .01f;
+    /// <summary>
+    /// Absolute maximum allowable acceleration. Should never be reached
+    /// </summary>
     [Range(-100f, 100f)] [SerializeField] private float accelerationMax = 100f;
+    /// <summary>
+    /// Factor of random rotation to allow player to flip around
+    /// </summary>
     [Range(0f, 1f)] [SerializeField] private float shake = 0.001f;
+    /// <summary>
+    /// The collider of the room the player resides in
+    /// </summary>
     [SerializeField] private LayerMask roomCollider;
+    /// <summary>
+    /// The rate at which the player can accelerate
+    /// </summary>
     [Range(0f, 50f)] [SerializeField] private float speed = 8.0f;
     private Vector2 acceleration;
     private Vector2 orientation;
