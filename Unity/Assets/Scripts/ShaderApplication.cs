d83cfc28980f3a20de8660feffc331cd22e13a18
RogueFirework <70032201+roguefirework@users.noreply.github.com>
2024-05-24 11:52:49 -0700

    Add shaders to rats (#74)
    Add shaders to the rats

---
+++ Unity/Assets/Scripts/ShaderApplication.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using UnityEngine;
+
+namespace WumpusUnity
+{
+    public class ShaderApplication : MonoBehaviour
+    {
+        [SerializeField]
+        private int width;
+        [SerializeField]
+        private int height;
+        [SerializeField]
+        private float magnitude;
+        [SerializeField]
+        private float speed;
+        [SerializeField] private float minTime;
+        [SerializeField] private float maxTime;
+        [SerializeField] private float OverallDistortionSpeed;
+        [SerializeField] private float OverallDistortionMag;
+        [SerializeField] private float overallDistortionFreq;
+
+        [SerializeField] private bool doDistortionPrePixelazation;
+        [SerializeField]
+        private Shader shader;
+        private Material material;
+        private static readonly int Width = Shader.PropertyToID("_Width");
+        private static readonly int Height = Shader.PropertyToID("_Height");
+        private static readonly int DistortionSpeed = Shader.PropertyToID("_DistortionSpeed");
+        private static readonly int DistortionMagnitude = Shader.PropertyToID("_DistortionMagnitude");
+        private static readonly int DistortionMinTime = Shader.PropertyToID("_DistortionMinTime");
+        private static readonly int DistortionMaxTime = Shader.PropertyToID("_DistortionMaxTime");
+
+        private static readonly int Lines = Shader.PropertyToID("_ScanLines");
+        private static readonly int LineWidth = Shader.PropertyToID("_ScanLineWidth");
+        private static readonly int DistortionMag = Shader.PropertyToID("_OverallDistortionMag");
+        private static readonly int DistortionFreq = Shader.PropertyToID("_OverallDistortionFrequency");
+        private static readonly int OverallDistortionChangeRate = Shader.PropertyToID("_OverallDistortionChangeRate");
+        private static readonly int DoDistortionAfterPixelization = Shader.PropertyToID("_DoDistortionAfterPixelization");
+
+        public ShaderApplication(float maxTime)
+        {
+            this.maxTime = maxTime;
+        }
+
+        public void Awake()
+        {
+            material = new Material(shader);
+        }
+
+        public void Update()
+        {
+            material.SetInteger(Width,width);
+            material.SetInteger(Height,height);
+            material.SetFloat(DistortionSpeed,speed);
+            material.SetFloat(DistortionMagnitude,magnitude);
+            material.SetFloat(DistortionMinTime,minTime);
+            material.SetFloat(DistortionMaxTime,maxTime);
+            material.SetFloat(DistortionMag,OverallDistortionMag);
+            material.SetFloat(DistortionFreq,overallDistortionFreq);
+
+            material.SetFloat(OverallDistortionChangeRate,OverallDistortionSpeed);
+            material.SetInteger(DoDistortionAfterPixelization,(doDistortionPrePixelazation) ? 1 : 0);
+
+        }
+
+        public void OnRenderImage(RenderTexture source, RenderTexture destination)
+        {
+            Graphics.Blit(source,destination,material);
+        }
+    }
+}
---
+++ Unity/Assets/Shaders/Pixalization shader.shader
@@ -0,0 +1,90 @@
+Shader "Hidden/Pixalization shader"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+        _Width ("Width", Integer) = 100
+        _Height ("Height", Integer) = 100
+        _DistortionSpeed ("Distortion", Float) = 1.0
+        _DistortionMagnitude ("Distortion Magnitude", Float) = 5.0
+        _DistortionMinTime ("Distortion Min", Float) = 1.0
+        _DistortionMaxTime ("Distortion Min", Float) = 3.0
+        _OverallDistortionMag ("Overall distortion", Float) = 20.0
+        _OverallDistortionFrequency ("Overall distortion frequency", Float) = 0.1
+        _OverallDistortionChangeRate ("Overall distortion change", Float) = 0.3
+        _DoDistortionAfterPixelization ("Distortion first", Integer) = 0
+    }
+    SubShader
+    {
+        // No culling or depth
+        Cull Off ZWrite Off ZTest Always
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.uv;
+                return o;
+            }
+
+            sampler2D _MainTex;
+            float _Width;
+            float _Height;
+            float _DistortionSpeed;
+            float _DistortionMagnitude;
+            float _DistortionMinTime;
+            float _DistortionMaxTime;
+            float _OverallDistortionMag;
+            float _OverallDistortionChangeRate;
+            int _DoDistortionAfterPixelization;
+            float _OverallDistortionFrequency;
+            fixed4 frag (v2f i) : SV_Target
+            {
+                float2 uv = i.uv;
+                float4 loopedTime = _Time % (_DistortionMaxTime - _DistortionMinTime) + _DistortionMinTime;
+                float2 distortAmount = float2 ((sin((uv.y + (_Time * _OverallDistortionChangeRate)) *_OverallDistortionFrequency) * _OverallDistortionMag).x,0);
+
+
+                float2 newRes = float2 (_Width, _Height) + (sin(loopedTime * _DistortionSpeed) * _DistortionMagnitude).xy;
+                uv = uv*newRes;
+                if (_DoDistortionAfterPixelization == 1)
+                {
+                    uv += distortAmount;
+                }
+                
+                uv = floor(uv);
+                if (_DoDistortionAfterPixelization == 0)
+                {
+                    uv += distortAmount;
+                }
+                uv /= newRes;
+
+                fixed4 col = tex2D(_MainTex, uv);
+                
+
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
