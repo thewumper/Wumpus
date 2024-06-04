39008681f52748ce25235d6b2706b2500ac46cc4
1118422ILHS <1118422@apps.nsd.org>
2024-06-03 19:57:06 -0700

    Finish basic spawner mode setup

--- Unity/Assets/Scripts/Battle/SpawnerController.cs
+++ Unity/Assets/Scripts/Battle/SpawnerController.cs
@@ -1,33 +1,50 @@
 using System;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace WumpusUnity.Battle
 {
-    public class SpawnerController
+    public class SpawnerController : MonoBehaviour
     {
         [SerializeField] private GameObject Room;
-        [SerializeField] private new Rigidbody2D Rigidbody;
+        [SerializeField] private Rigidbody2D Rigidbody;
         [SerializeField] private GameObject Prefab;
         
-        /// <summary>
-        /// Pairs of spawner mode components and the time they remain when active
-        /// </summary>
-        [SerializeField] private (SpawnerMode, float)[] Modes;
+        private SpawnerMode[] Modes;
+        private int currentMode = 0;
 
         private float remainingTime;
 
         void Start()
         {
-            foreach ((SpawnerMode, float) mode in Modes)
+            Modes = GetComponents<SpawnerMode>();
+            foreach (SpawnerMode mode in Modes)
             {
-                mode.Item1.Initialize(Room, Rigidbody, Prefab);
+                mode.Initialize(Room, this.Rigidbody, Prefab);
             }
+
+            // Mode decided in Update
+            remainingTime = 0f;
         }
         
         void Update()
         {
-            //
+            remainingTime -= Time.deltaTime;
+            if (remainingTime <= 0)
+            {
+                Modes[currentMode].enabled = false;
+                int nextMode = currentMode;
+                while (nextMode == currentMode)
+                {
+                    nextMode = UnityEngine.Random.Range(0, Modes.Length);
+                }
+
+                currentMode = nextMode;
+                Debug.Log("Switching to mode " + currentMode + ", " + Modes[currentMode].name);
+                Modes[currentMode].enabled = true;
+                remainingTime = Modes[currentMode].duration;
+            }
         }
-        
     }
 }
--- Unity/Assets/Scripts/Battle/SpawnerMode.cs
+++ Unity/Assets/Scripts/Battle/SpawnerMode.cs
@@ -5,14 +5,16 @@ namespace WumpusUnity.Battle
     public class SpawnerMode : MonoBehaviour
     {
         protected GameObject Room;
-        protected new Rigidbody2D Rigidbody;
+        protected Rigidbody2D Rigidbody;
         protected GameObject Prefab;
+        [SerializeField] public float duration;
 
         public void Initialize(GameObject room, Rigidbody2D rigidbody, GameObject bullet)
         {
             this.Room = room;
             this.Rigidbody = rigidbody;
             this.Prefab = bullet;
+            this.enabled = false;
         }
     }
 }
