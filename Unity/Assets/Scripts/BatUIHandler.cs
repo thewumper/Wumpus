0e638803b2d4510ec6c17cbadfd78155072efef6
MaximumMaxxx <maxwellwindland@gmail.com>
2024-06-04 15:02:27 -0700

    Bats are kinda done

--- Unity/Assets/Scripts/BatUIHandler.cs
+++ Unity/Assets/Scripts/BatUIHandler.cs
@@ -1,10 +1,19 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 using WumpusCore.Controller;
 using WumpusUnity;
+using Random = UnityEngine.Random;
 
 public class BatUIHandler : MonoBehaviour
 {
+    [SerializeField] private GameObject batModel;
+
+
+    [SerializeField] private float size = 0.05f;
+
+    [SerializeField] private double batMultiplier = 1.1;
+
     /// <summary>
     /// The game object for the camera
     /// </summary>
@@ -40,6 +49,26 @@ public class BatUIHandler : MonoBehaviour
     /// </summary>
     private const float camSens = 5f;
 
+    private void Start()
+    {
+        // Initializes the movingID.
+        fadingID = Animator.StringToHash("fading");
+        movingAnimator.SetBool(fadingID, false);
+
+        // Instantiates the Controller, if there isn't one already.
+        try
+        {
+            controller = Controller.GlobalController;
+        }
+        catch (NullReferenceException)
+        {
+            controller = new Controller
+                (Application.dataPath + "/Trivia/Questions.json", Application.dataPath + "/Maps", 0);
+        }
+
+        controller.Debug = true;
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -49,6 +78,34 @@ public class BatUIHandler : MonoBehaviour
         cam.transform.eulerAngles += new Vector3(0, mouseX * camSens, 0);
         // PersistentData.Instance.EulerAngle = cam.transform.eulerAngles;
 
-        movingAnimator.SetBool(fadingID, true);
+        if (numBats > 100)
+        {
+            movingAnimator.SetBool(fadingID, true);
+        }
+
+
+
+        if (movingAnimator.GetBool(fadingID))
+        {
+            // If the screen has fully faded to black.
+            if (black.color.a.Equals(1))
+            {
+                controller.ExitBat();
+                SceneController.GlobalSceneController.GotoCorrectScene();
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (numBats < 300)
+        {
+
+        // This could use instanced rendering for performance if needed
+        Vector3 randomSpawnPosition = new Vector3(Random.Range(-10, 11), 5, Random.Range(-10, 11));
+        GameObject instance =  Instantiate(batModel, randomSpawnPosition, Quaternion.identity);
+        instance.gameObject.transform.localScale = new Vector3(size,size,size);
+        numBats++;
+        }
     }
 }
