1f3a8bf35a6d13adfbf0e2b8720df96b21339b2f
Duck <113476107+ilhs1096069@users.noreply.github.com>
2024-06-06 12:37:18 -0700

    Cool Looking Vats (#89)
    * el goblino esta aqui
    
    * Better lighting
    
    * *LOUD INCORRECT BUZZER*
    
    and some lighting changes and stuff
    
    * LOUD INCORRECT BUZZER working properly
    
    * Fix lighting in bad scenes
    
    * back to standard room spawning

--- Unity/Assets/Scripts/VatUI.cs
+++ Unity/Assets/Scripts/VatUI.cs
@@ -10,6 +10,7 @@ public class VatUI : MonoBehaviour
     void Awake()
     {
         // This is a mega placeholder file. Just switch scenes and let trivia deal with it.
+        // I guess we're gonna have it like this forever because I do not have time to fully implement a vat room
         Controller.GlobalController.StartTrivia();
         SceneController.GlobalSceneController.GotoCorrectScene();
     }
