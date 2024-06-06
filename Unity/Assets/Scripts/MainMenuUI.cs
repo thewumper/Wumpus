849d59154af8f42ebe8136050647903842430325
Jayder <113476107+ilhs1096069@users.noreply.github.com>
2024-06-06 11:10:00 -0700

    back to standard room spawning

--- Unity/Assets/Scripts/MainMenuUI.cs
+++ Unity/Assets/Scripts/MainMenuUI.cs
@@ -40,7 +40,7 @@ public class MainMenuUI : MonoBehaviour
         Debug.Log("Loading map #" + randomMap);
 
         // Initializes the Controller.
-        controller = new Controller(triviaNormal, topologyDir, randomMap, 0,5,0,0,5,5,4000);
+        controller = new Controller(triviaNormal, topologyDir, randomMap);
         // Initializes the SceneController.
         sceneController = SceneController.GlobalSceneController;
         sceneController.Reinitialize(controller);
